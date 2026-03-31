2e17fe4823f39837f68f142d28cf4427ab245cc9
Breno Silva <109807534+brenosilldev@users.noreply.github.com>
2026-03-31 18:09:09 -0300

    Create src/config/constants.js for Smart-Git configuration management

---
+++ src/config/constants.js
@@ -0,0 +1,29 @@
+// Constants for Smart-Git configuration management
+
+/**
+ * Commit Types:
+ * - feat: A new feature
+ * - fix: A bug fix
+ * - docs: Documentation only changes
+ * - style: Changes that do not affect the meaning of the code (white-space, formatting, etc)
+ * - refactor: A code change that neither fixes a bug nor adds a feature
+ * - perf: A code change that improves performance
+ * - test: Adding missing or correcting existing tests
+ * - chore: Changes to the build process or auxiliary tools and libraries such as documentation generation
+ * 
+ * Commit Patterns:
+ * The following pattern is used for commit messages: <type>(<scope>): <subject>
+ */
+
+const COMMIT_TYPES = {
+    feat: 'A new feature',
+    fix: 'A bug fix',
+    docs: 'Documentation only changes',
+    style: 'Changes that do not affect the meaning of the code',
+    refactor: 'A code change that neither fixes a bug nor adds a feature',
+    perf: 'A code change that improves performance',
+    test: 'Adding missing or correcting existing tests',
+    chore: 'Changes to the build process or auxiliary tools'
+};
+
+module.exports = COMMIT_TYPES;
